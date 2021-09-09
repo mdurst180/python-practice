0a13a5a88e0582ef3dba73f916aae7734ee58b7d
Mike Durst <mdurst@helloinspire.com>
2021-09-09 11:22:37 -0400

    add more tests

--- test.py
+++ test.py
@@ -7,5 +7,11 @@ class TestSum(unittest.TestCase):
     def test_diff21(self):
         self.assertEqual(diff21(21), 0, "Should be 0")
 
+    def test_diff21_lessthan21(self):
+        self.assertEqual(diff21(20), 1, "Should be 1")
+
+    def test_diff21_greaterthan21(self):
+        self.assertEqual(diff21(22), 2, "Should be 2")
+
 if __name__ == '__main__':
     unittest.main()
