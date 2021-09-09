18c5f15cb3c79aa28babdd59ddf11ac6eafb6ac1
Mike Durst <mdurst@helloinspire.com>
2021-09-09 14:41:51 -0400

    add near_hundred

--- warmups/basics-tests.py
+++ warmups/basics-tests.py
@@ -12,5 +12,8 @@ class TestBasics(unittest.TestCase):
     def test_diff21_greaterthan21(self):
         self.assertEqual(basics.diff21(22), 2, "Should be 2")
 
+    def test_near_hundred_95(self):
+        self.assertEqual(basics.near_hundred(22), True, "Should be True")
+
 if __name__ == '__main__':
     unittest.main()
--- warmups/basics.py
+++ warmups/basics.py
@@ -4,3 +4,10 @@ def diff21(n):
         return 21 - n
     else:
         return (n - 21) * 2
+
+# Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+# near_hundred(93) → True
+# near_hundred(90) → True
+# near_hundred(89) → False
+def near_hundred(n):
+    return True
